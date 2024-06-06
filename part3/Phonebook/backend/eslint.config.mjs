d0031f0a379d35414f42d29e83574cf33f23e1a3
LIU0611 <yuxuanliu_1999@163.com>
2024-06-07 01:48:48 +0300

    complete part3 exercises 1-22

--- part3/Phonebook/backend/eslint.config.mjs
+++ part3/Phonebook/backend/eslint.config.mjs
@@ -1,9 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs", globals: globals.node}}, // Use `globals.node` to include Node.js globals
   pluginJs.configs.recommended,
 ];
