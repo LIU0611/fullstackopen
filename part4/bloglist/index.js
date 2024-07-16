45e5ccb7aea72dd9c51ed6d09685b2005d4ca19a
LIU0611 <yuxuanliu_1999@163.com>
2024-07-16 17:29:54 +0300

    start work on part4

---
+++ part4/bloglist/index.js
@@ -0,0 +1,35 @@
+// index.js
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+
+const mongoUrl = 'mongodb+srv://LIU0611:19990611@cluster0.hhxdhcz.mongodb.net/part4?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
