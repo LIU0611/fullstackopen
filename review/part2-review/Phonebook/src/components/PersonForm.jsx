abebc48f5c8b7ee83ea6b844731905c4e78e878f
LIU0611 <yuxuanliu_1999@163.com>
2024-10-07 19:48:46 +0300

    Review: Fullstack Part 2 Forms

---
+++ review/part2-review/Phonebook/src/components/PersonForm.jsx
@@ -0,0 +1,23 @@
+const PersonForm = ({addPerson, newName, setNewName, newNumber, setNewNumber}) =>
+{
+    return (
+        <form>
+        <div>
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+
+        <div>
+          number: {""}
+          <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+
+        <div>
+          <button type="submit" onClick={addPerson}>
+            add
+          </button>
+        </div>
+      </form>
+    )
+}
+
+export default PersonForm;
---
+++ review/part2-review/Phonebook/src/components/Persons.jsx
@@ -0,0 +1,16 @@
+
+
+const Persons = ({personsToShow}) => {
+
+  return (
+    <ul>
+      {personsToShow.map((person, index) => (
+        <li key={index}>
+          {person.name} {person.number}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Persons;
